e75cd771eca5ecd3a4b299d2d75ce5759fa15829
Fernando Cebrián <cebrianorama@gmail.com>
2022-01-30 17:43:01 +0100

    Ejercicio calculo dni completo

--- calculo_letra_dni/index.php
+++ calculo_letra_dni/index.php
@@ -6,9 +6,9 @@ $error=$letra_dni="";
 if(isset($_POST['calcula'])){
 
     if($dni=filter_input(INPUT_POST,'dni',FILTER_VALIDATE_INT)){
-        $letra_dni="La letra del DNI introducido es: ".calculaletra($dni);
+        $letra_dni="La letra del DNI introducido es la <b>".calculaletra($dni)."</b>";
     }else{
-        $error="Por favor, debes introducir sólo valores enteros";
+        $error="Por favor, debes introducir sólo valores numéricos";
     }
     
 }
@@ -30,6 +30,7 @@ function calculaletra($dni){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos.css">
     <title>Cálculo de la letra del DNI</title>
 </head>
 <body>
@@ -39,13 +40,13 @@ function calculaletra($dni){
         
         <form action="." method="post">
             <h2>Introduce las 8 cifras del DNI</h2>
-            <input type="text" name="dni" id="dni" maxlength="8" minlength="8">
-            <button id="calcula" name="calcula">Calcular letra</button>
+            <p><input type="text" name="dni" id="dni" maxlength="8" minlength="8"></p>
+            <p class="error"><?php echo $error ?></p>
+            <p><button id="calcula" name="calcula">CALCULAR LETRA</button></p>
         </form>
         <p>Ningún dato introducido para calcular la letra del DNI será almacenado</p>
-        <p class="error"><?php echo $error ?></p>
         <p class="resultado"><?php echo $letra_dni ?></p>
-    <main>
+    </main>
 
 </body>
 </html>
